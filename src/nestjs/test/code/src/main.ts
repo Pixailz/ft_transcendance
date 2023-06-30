c957a714effea0563b1c72936a3cc7176ef1f8a1
Nicolas <koala@coucou.fr>
2023-06-30 20:02:09 +0200

    fix psql container and add nest code

---
+++ src/nestjs/test/code/src/main.ts
@@ -0,0 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app: NestExpressApplication = await NestFactory.create(AppModule);
+  const config: ConfigService = app.get(ConfigService);
+  const port: number = config.get<number>('PORT');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(port, () => {
+    console.log('[WEB]', config.get<string>('BASE_URL'));
+  });
+}
+
+bootstrap();
