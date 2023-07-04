671fc15cf00afb0d22f59c0055c4587b94b6da86
clinche <101885891+clinche@users.noreply.github.com>
2023-07-04 18:45:26 -0400

    clean main branch

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from "@nestjs/config";
 
-import { api42OAuthModule } from "src/api42OAuth/api42OAuth.module";
-import { DbModule} from "src/db/db.module";
-
 @Module({
 	imports: [
-		api42OAuthModule,
-		DbModule
 	],
 	controllers: [],
 	providers: [],
