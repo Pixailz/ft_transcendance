8f6f7e778122ddc88070e72d318556f75da83adc
gab <ldournoi@student.42angouleme.fr>
2023-07-04 18:50:42 -0400

    ✨[backend] added api42OAuth to exported modules

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from "@nestjs/config";
 
+import { api42OAuthModule } from './api42OAuth/api42OAuth.module';
+
 @Module({
 	imports: [
+		api42OAuthModule
 	],
 	controllers: [],
 	providers: [],
