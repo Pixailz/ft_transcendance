78e1e008e942801af5ef18a94be09fa32560879f
Louis-Gabriel Dournois <ldournoi@student.42angouleme.fr>
2023-07-05 03:25:24 +0200

    :construction_crane: began working on landing page component

---
+++ src/angularjs/src/app/landingcomponent/landingcomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landingcomponent',
+  templateUrl: './landingcomponent.component.html',
+  styleUrls: ['./landingcomponent.component.scss']
+})
+export class LandingcomponentComponent {
+
+}
