54915b854a14adaa7389ffef6a9a4cd89626fdd0
Pixailz <pixailz1@gmail.com>
2023-07-05 02:30:49 -0400

    :package: cookie.

--- src/nestjs/src/modules/api42OAuth/api42OAuth.module.ts
+++ src/nestjs/src/modules/api42OAuth/api42OAuth.module.ts
@@ -3,10 +3,18 @@ import { Module } from "@nestjs/common";
 import { Api42OAuthController } from "./api42OAuth.controller";
 import { Api42OAuthService } from "./api42OAuth.service";
 import { ConfigModule } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
+		JwtModule.register({
+			global: true,
+			secretOrPrivateKey: process.env.JWT_SECRET,
+			signOptions: {
+				expiresIn: "2h",
+			},
+		}),
 	],
 	controllers: [Api42OAuthController],
 	providers: [Api42OAuthService],
