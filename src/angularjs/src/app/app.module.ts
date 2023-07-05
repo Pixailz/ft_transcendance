f55cfde02b056bf576f13234be603c415ed6956b
Louis-Gabriel Dournois <ldournoi@student.42angouleme.fr>
2023-07-05 03:41:36 +0200

    :construction_crane: trying to put together header, footer, dashboard and landing page

--- src/angularjs/src/app/app.module.ts
+++ src/angularjs/src/app/app.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LandingcomponentComponent } from './landingcomponent/landingcomponent.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingcomponentComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeDashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
