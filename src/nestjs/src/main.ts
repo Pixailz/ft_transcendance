71b75bc6dbe67b4c8e5bac2188658c97d40aa0e0
Pixailz <pixailz1@gmail.com>
2023-07-08 17:39:32 +0200

    :construction: [main.ts] dto validation config

--- src/nestjs/src/main.ts
+++ src/nestjs/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(new ValidationPipe({
+		// Strip not specified field in post
+		whitelist: true,
+		// automatic strip for js object
+		transform: true,
+	}));
 	await app.listen(3000);
 }
 bootstrap();
