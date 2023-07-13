72f7dd5c95783c139a53abfc4ef680f6d7eea6c8
gab <ldournoi@student.42angouleme.fr>
2023-07-13 07:51:31 -0400

    undo auth and restore branch to working state

--- src/angularjs/src/app/pages/user-profile/user-profile.component.ts
+++ src/angularjs/src/app/pages/user-profile/user-profile.component.ts
@@ -1,23 +1,12 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../../../projects/auth/src/lib/auth.service';
 
 @Component({
 	selector: 'app-user-profile',
 	templateUrl: './user-profile.component.html',
 	styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent {
-  constructor() { }
-  public userGuard: AuthService = new AuthService();
-	public userLoggedIn: boolean = false;
-  public user = null;
 
-  ngOnInit(): void {
-    this.userLoggedIn = this.userGuard.isAuthenticated();
-    if (this.userLoggedIn) {
-      this.user = this.userGuard.getUser();
-    }
-    else
-      this.user = null;
+export class UserProfileComponent {
+  constructor() {
   }
 }
