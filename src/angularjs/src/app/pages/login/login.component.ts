932433a28c4155a1cffee6f3474205b7aebf30d9
gab <ldournoi@student.42angouleme.fr>
2023-07-15 01:29:02 +0200

    updated jwt auth request link

--- src/angularjs/src/app/pages/login/login.component.ts
+++ src/angularjs/src/app/pages/login/login.component.ts
@@ -20,17 +20,22 @@ export class LoginComponent  implements OnInit {
 		this.code = this.route.snapshot.queryParamMap.get('code');
 
 		if (this.code !== null) {
+      this.getToken();
+		}
+	}
+
+  async getToken() {
       try {
-	  		this.http.get('http://localhost:3000/auth/ft_callback?code=' + this.code).subscribe(data => {
-	  			this.response = data;
-         localStorage.setItem('access_token', this.response.access_token);
-	  		});
+	  		this.http.get('http://localhost:4200/auth/ft_callback?code=' + this.code).subscribe((data) => {
+          this.response = data;
+          console.log(this.response);
+          localStorage.setItem('access_token', this.response.access_token);
+			    window.location.href = 'http://localhost:4200';
+        });
       } catch (e) {
         console.log(e);
       }
-			window.location.href = 'http://localhost:4200';
-		}
-	}
+  }
 
 	SignIn() {
 		window.location.href = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d051dd7a35b0f79ee848231784c70f568b325bfe7cda78468e9e1b142bd511e2&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Flogin&response_type=code';
