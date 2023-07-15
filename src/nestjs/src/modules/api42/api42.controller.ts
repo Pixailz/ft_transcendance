1615801877f6df93a8fbee8a27dd667a58a14d5e
Pixailz <pixailz1@gmail.com>
2023-07-15 07:17:56 +0200

    unecessary controller

--- src/nestjs/src/modules/api42/api42.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from "@nestjs/common";
-
-@Controller("api42")
-export class Api42Controller {}
--- src/nestjs/src/modules/api42/api42.module.ts
+++ src/nestjs/src/modules/api42/api42.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
 import { Api42Service } from "./api42.service";
-import { Api42Controller } from "./api42.controller";
 
 @Module({
 	providers: [Api42Service],
-	controllers: [Api42Controller],
+	controllers: [],
 	exports: [Api42Service],
 })
 export class Api42Module {}
