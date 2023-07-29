df912096ce788cb5f39cfa683ab90c859c6bcd05
Nicolas <koala@coucou.fr>
2023-07-29 14:31:37 +0200

    update relation

--- src/nestjs/src/modules/database/gameInfo/entity.ts
+++ src/nestjs/src/modules/database/gameInfo/entity.ts
@@ -3,22 +3,26 @@ import {
 	ManyToOne,
 	Column,
 	JoinColumn,
-	PrimaryColumn
+	PrimaryColumn,
+	PrimaryGeneratedColumn
 } from "typeorm";
 
 import { UserEntity } from "../user/entity";
 
 @Entity()
 export class GameInfoEntity {
+	@PrimaryGeneratedColumn()
+	public id: number;
+
 	@PrimaryColumn()
 	public userA: number;
 	public userB: number;
 
-	@ManyToOne(type => UserEntity, user => user.roomInfo)
+	@ManyToOne(type => UserEntity, user => user.gameUserA)
 	@JoinColumn({ name: "userA" })
 	firstUser: UserEntity;
 
-	@ManyToOne(type => UserEntity, user => user.roomInfo)
+	@ManyToOne(type => UserEntity, user => user.gameUserB)
 	@JoinColumn({ name: "userB" })
 	secondUser: UserEntity;
 
