a44c4d53585c0264f68b6fad902537e20fea46b4
Nicolas <koala@coucou.fr>
2023-07-29 18:17:35 +0200

    separate UserPost for create User and UserInfoPost for update user

--- src/nestjs/src/modules/database/user/dto.ts
+++ src/nestjs/src/modules/database/user/dto.ts
@@ -1,17 +1,25 @@
-import { IsNotEmpty, IsOptional, IsEmail, IsString} from "class-validator";
+import { IsNotEmpty, IsOptional, IsEmail, IsString, IsBooleanString} from "class-validator";
+import { isBooleanObject } from "util/types";
 
 export class UserPost {
+	@IsNotEmpty()
 	@IsString()
 	ftLogin?: string;
 }
 
 export class UserInfoPost {
+	@IsNotEmpty()
 	@IsString()
 	nickname?: string;
 
+	@IsNotEmpty()
 	@IsEmail()
 	email?: string;
 	
 	@IsOptional()
 	picture?: string;
+
+	// @IsOptional()
+	// @IsBooleanString()
+	// twoAuthFactor?: boolean
 }
