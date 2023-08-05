57067d39bf2234bc0c08d33a7130dc4390174fca
Pixailz <pixailz1@gmail.com>
2023-08-05 19:05:57 +0200

    [front] add environments file - package.json add correct configuration options - angular.json add fileReplacements for dev config - update login.component

---
+++ src/angularjs/src/app/environments/environment.dev.ts
@@ -0,0 +1,3 @@
+export const environment = {
+	after_auth_uri: "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d051dd7a35b0f79ee848231784c70f568b325bfe7cda78468e9e1b142bd511e2&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Flogin&response_type=code"
+};
--- src/angularjs/src/app/pages/login/login.component.ts
+++ src/angularjs/src/app/pages/login/login.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from 'src/app/environments/environment';
+
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
@@ -39,6 +41,6 @@ export class LoginComponent  implements OnInit {
 
 	SignIn()
 	{
-		window.location.href = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d051dd7a35b0f79ee848231784c70f568b325bfe7cda78468e9e1b142bd511e2&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Flogin&response_type=code';
+		window.location.href = environment.after_auth_uri;
 	};
 }
