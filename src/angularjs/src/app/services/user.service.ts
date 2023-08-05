c09c0d0e64cbc08759e9cded0fa657064a8d04a8
Pixailz <pixailz1@gmail.com>
2023-08-05 18:13:47 +0200

    [front] add reqService to userService

--- src/angularjs/src/app/services/user.service.ts
+++ src/angularjs/src/app/services/user.service.ts
@@ -1,14 +1,12 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { reqService } from "./back-req.service";
 
 @Injectable({
-	providedIn: 'root',
+	providedIn: "root",
 })
 export class userService {
-	constructor(private router: Router, private http: HttpClient) {  }
+	constructor(private ReqService: reqService) {  }
 	private userLoggedIn = false;
-	response: any = null;
 
 	isLoggedIn()
 	{
@@ -25,34 +23,19 @@ export class userService {
 		window.location.href = "/home";
 	}
 
-	async getUserInfo()
+	async getUserInfo(): Promise<any>
 	{
-		const jwt_token = localStorage.getItem("access_token");
-		const req = await fetch('/api/user/me', {
-			headers: {"Authorization": "Bearer " + jwt_token}
-		});
-		if (req.status !== 200)
-			return Promise.reject({status: "no user"});
-		return Promise.resolve(req.json());
+		return (await this.ReqService.Back("GET", "/api/user/me"));
 	}
 
 	async updateInfo(nickname: string, email?: string)
 	{
-		const jwt_token = localStorage.getItem("access_token");
 		let body;
+
 		if (email)
-			body = JSON.stringify({'nickname' : nickname, 'email' : email});
+			body = JSON.stringify({"nickname" : nickname, "email" : email});
 		else
-			body = JSON.stringify({'nickname' : nickname});
-		let res:any = await fetch('/api/user/me', {
-			method: 'PUT',
-			headers: {
-				'Authorization': 'Bearer ' + jwt_token,
-				'Content-Type': 'application/json'
-			},
-			body: body,
-			mode: 'cors'
-		});
-		return res.status;
+			body = JSON.stringify({"nickname" : nickname});
+		return (await this.ReqService.Back("PUT", "/api/user/me", body));
 	}
 }
