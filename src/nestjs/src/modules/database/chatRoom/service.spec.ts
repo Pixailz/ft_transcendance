8bc19705882d058e963a85bae4bfe0b4c1c4371a
Nicolas <koala@coucou.fr>
2023-08-17 16:05:46 +0200

    create unit test for user and chatRoom service

---
+++ src/nestjs/src/modules/database/chatRoom/service.spec.ts
@@ -0,0 +1,86 @@
+import { DBChatRoomService } from './service';
+import { ChatRoomEntity } from './entity';
+import { Repository } from 'typeorm';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ForbiddenException } from '@nestjs/common';
+import { DBModule } from '../database.module';
+
+ describe('DBChatRoomService', () => {
+  let service: DBChatRoomService;
+  let repo: Repository<ChatRoomEntity>;
+  let unit_room: string = "UNIT_room";
+  let unit_room_bis = unit_room + "_BIS";
+
+  beforeEach(async () => {
+	const module = await Test.createTestingModule({
+	  imports: [DBModule],
+	  providers: [
+		DBChatRoomService,
+		{
+		  provide: getRepositoryToken(ChatRoomEntity),
+		  useClass: Repository,
+		},
+	  ],
+	}).compile();
+	
+	service = module.get<DBChatRoomService>(DBChatRoomService);
+	repo = module.get<Repository<ChatRoomEntity>>(getRepositoryToken(ChatRoomEntity));
+  });
+  
+  it('should be defined', () => {
+	expect(service).toBeDefined();
+  });
+  
+	describe('testing create and returnOne', () => {
+		it('[CHATROOM] should create 2 room and update', async () => {
+			const test = await service.returnAll();
+			if (test.length != 0)
+			{
+				const len = test.length;
+				unit_room += len - 1; 
+				unit_room_bis = unit_room + "_BIS";
+			}
+			const roomPost = { name: unit_room };
+			const roomPost2 = { name: unit_room_bis};
+
+			let roomId = await service.create(roomPost);      
+			let room = await service.returnOne(roomId);  
+			console.log('in create first room : room id ', room.id, 'room login = ', room.name);
+			expect(roomId).toEqual(room.id);
+
+			let roomId2 = await service.create(roomPost2);
+			let room2 = await service.returnOne(roomId2);  
+			console.log('in create second room : room id ', room2.id, 'room login = ', room2.name);
+			expect(roomId2).toEqual(room2.id);
+		});
+	  });
+	   
+	describe('update', () => {
+		it('[CHATROOM] should update 2 room', async () => {
+			const roomPost = { name: unit_room + '_name', type : "random_type"};
+			const roomPost2 = { name: unit_room + '_name_BIS', type : "random_type"};
+			let room_id = (await repo.findOne({where: {name: unit_room}})).id
+			let room2_id = (await repo.findOne({where: {name: unit_room_bis}})).id
+			await service.update(room_id, roomPost);
+			await service.update(room2_id, roomPost2);
+			const room = await service.returnOne(room_id);
+			const room2 = await service.returnOne(room2_id);
+			expect(room.name).toEqual(roomPost.name);
+			expect(room2.name).toEqual(roomPost2.name);
+		});
+	  });
+
+	describe('delete', () => {
+		it('should delete a room', async () => {
+			const room = await repo.findOneBy({name: unit_room + '_name_BIS'}); 
+			console.log('room in delete = ', room);
+			const id = room.id;
+			await service.delete(id);
+			// expect(await service.returnOne(id)).toThrowError(new ForbiddenException("ChatRoom not found"));
+			await expect(service.returnOne(id)).rejects.toThrowError(
+				new ForbiddenException("ChatRoom not found"),
+				);
+		});
+	});  
+});
---
+++ src/nestjs/src/modules/database/user/service.spec.ts
@@ -0,0 +1,90 @@
+import { DBUserService } from './service';
+import { UserEntity } from './entity';
+import { Repository } from 'typeorm';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ForbiddenException } from '@nestjs/common';
+import { DBModule } from '../database.module';
+
+ describe('DBUserService', () => {
+  let service: DBUserService;
+  let repo: Repository<UserEntity>;
+  let unit_user: string = "UNIT_USER";
+  let unit_user_bis = unit_user + "_BIS";
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [DBModule],
+      providers: [
+        DBUserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: Repository,
+        },
+      ],
+    }).compile();
+    
+    service = module.get<DBUserService>(DBUserService);
+    repo = module.get<Repository<UserEntity>>(getRepositoryToken(UserEntity));
+  });
+  
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  
+  describe('testing create and returnOne', () => {
+    it('[USER] should create 2 user and 1 error (empty ftLogin)', async () => {
+        const test = await service.returnAll();
+        if (test.length != 0)
+        {
+          const len = test.length;
+          unit_user += len - 1; 
+          unit_user_bis = unit_user + "_BIS";
+        }
+        const userPost = { ftLogin: unit_user };
+        const userPost2 = { ftLogin: unit_user_bis};
+        const userPost3 = { ftLogin: "   "};
+
+        let userId = await service.create(userPost);      
+        let user = await service.returnOne(userId, null);  
+        // console.log('in create first user : user id ', user.id, 'user login = ', user.ftLogin);
+        expect(userId).toEqual(user.id);
+
+        let userId2 = await service.create(userPost2);
+        let user2 = await service.returnOne(userId2, null);  
+        // console.log('in create second user : user id ', user2.id, 'user login = ', user2.ftLogin);
+        expect(userId2).toEqual(user2.id);
+        
+        await expect(service.create(userPost3)).rejects.toThrowError(
+          new ForbiddenException("User Login can't be blank or empty"),
+          );
+        });
+      });
+       
+      describe('update', () => {
+        it('[USER] should update 2 user', async () => {
+          const userPost = { nickname: unit_user + '_name', email: unit_user + "@mail.com" };
+          const userPost2 = { nickname: unit_user + 'name_BIS', email: unit_user + "_BIS@mail.com" };
+          let user_id = (await service.returnOne(null, unit_user)).id;
+          let user2_id = (await service.returnOne(null, unit_user_bis)).id;
+          await service.update(user_id, userPost);
+          await service.update(user2_id, userPost2);
+          const user = await service.returnOne(user_id, null);
+          const user2 = await service.returnOne(user2_id, null);
+          expect(user.nickname).toEqual(userPost.nickname);
+          expect(user2.nickname).toEqual(userPost2.nickname);
+        });
+      });
+
+      describe('delete', () => {
+        it('should delete a user', async () => {
+          const user = await service.returnOne(null, unit_user_bis);
+          console.log('user in delete = ', user);
+          const id = user.id;
+          await service.delete(user.id);
+          const ret = await service.returnOne(id, null);
+          expect(ret).toBeNull();
+        });
+      });  
+
+    });
