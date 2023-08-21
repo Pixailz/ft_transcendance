294633b541f162d7985aa0631e4a64028fe85058
Ycaro02 <ycaro02@hotmail.fr>
2023-08-21 13:43:04 +0200

    add muted service unit test

--- src/nestjs/src/modules/database/muted/service.spec.ts
+++ src/nestjs/src/modules/database/muted/service.spec.ts
@@ -1,83 +1,88 @@
-// import { DBChatRoomService } from './service';
-// import { ChatRoomEntity } from './entity';
-// import { Repository } from 'typeorm';
-// import { Test } from '@nestjs/testing';
-// import { getRepositoryToken } from '@nestjs/typeorm';
-// import { ForbiddenException } from '@nestjs/common';
-// import { DBModule } from '../database.module';
+import { DBMutedService } from './service';
+import { MutedEntity } from './entity';
+import { Repository } from 'typeorm';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ForbiddenException } from '@nestjs/common';
+import { DBModule } from '../database.module';
+import { DBUserService } from '../user/service';
+import { UserEntity } from '../user/entity';
+import { exec } from 'child_process';
 
-//  describe('DBChatRoomService', () => {
-//   let service: DBChatRoomService;
-//   let repo: Repository<ChatRoomEntity>;
-//   let unit_room: string = "UNIT_ROOM";
-//   let unit_room_bis = unit_room + "_BIS";
-
-//   beforeEach(async () => {
-// 	const module = await Test.createTestingModule({
-// 	  imports: [DBModule],
-// 	  providers: [
-// 		DBChatRoomService,
-// 		{
-// 		  provide: getRepositoryToken(ChatRoomEntity),
-// 		  useClass: Repository,
-// 		},
-// 	  ],
-// 	}).compile();
-	
-// 	service = module.get<DBChatRoomService>(DBChatRoomService);
-// 	repo = module.get<Repository<ChatRoomEntity>>(getRepositoryToken(ChatRoomEntity));
-//   });
+ describe('DBMutedService', () => {
+  let service: DBMutedService;
+  let repo: Repository<MutedEntity>;
+  let unit_user: string = "UNIT_USER";
+  let userService: DBUserService;
+  beforeEach(async () => {
+	const module = await Test.createTestingModule({
+	  imports: [DBModule],
+	  providers: [
+		DBMutedService,
+		{
+		  provide: getRepositoryToken(MutedEntity),
+		  useClass: Repository,
+        },
+        DBUserService,
+        {
+            provide: getRepositoryToken(UserEntity),
+            useClass: Repository,
+        },
+	  ],
+	}).compile();
+	userService = module.get<DBUserService>(DBUserService);
+	service = module.get<DBMutedService>(DBMutedService);
+	repo = module.get<Repository<MutedEntity>>(getRepositoryToken(MutedEntity));
+  });
   
-//   it('should be defined', () => {
-// 	expect(service).toBeDefined();
-//   });
+  it('should be defined', () => {
+	expect(service).toBeDefined();
+  });
   
-// 	describe('testing create and returnOne', () => {
-// 		it('[CHATROOM] should create 2 room and update', async () => {
-// 			const test = await service.returnAll();
-// 			if (test.length != 0)
-// 			{
-// 				const len = test.length;
-// 				unit_room += len - 1; 
-// 				unit_room_bis = unit_room + "_BIS";
-// 			}
-// 			const roomPost = { name: unit_room };
-// 			const roomPost2 = { name: unit_room_bis};
+	describe('testing create and returnOne', () => {
+		it('[Muted] should create 2 Muted', async () => {
+			const test = await service.returnAll();
+		    const user_test = await userService.returnAll();
+            let len = 0;
+            if (user_test.length != 1)
+            {
+                len = user_test.length;
+                unit_user += len - 2; 
+            }
+            const me = await userService.returnOne(null, unit_user);
 
-// 			let roomId = await service.create(roomPost);      
-// 			let room = await service.returnOne(roomId);  
-// 			// console.log('in create first room : room id ', room.id, 'room login = ', room.name);
-// 			expect(roomId).toEqual(room.id);
-
-// 			let roomId2 = await service.create(roomPost2);
-// 			let room2 = await service.returnOne(roomId2);  
-// 			expect(roomId2).toEqual(room2.id);
-// 		});
-// 	  });
+			let Muted_id = await userService.create({ftLogin: unit_user + "Muted_test"});      
+			let tmp = await service.create({mutedId: Muted_id}, me.id); 
+			// console.log('in create first room : room id ', room.id, 'room login = ', room.name);
+			expect(tmp.mutedId).toEqual(Muted_id);
+			expect(tmp.meId).toEqual(me.id);
+			await userService.delete(Muted_id);
+		});
+	  });
 	   
-// 	describe('update', () => {
-// 		it('[CHATROOM] should update 2 room', async () => {
-// 			const roomPost = { name: unit_room + '_name', type : "random_type"};
-// 			const roomPost2 = { name: unit_room + '_name_BIS', type : "random_type"};
-// 			let room_id = (await repo.findOne({where: {name: unit_room}})).id
-// 			let room2_id = (await repo.findOne({where: {name: unit_room_bis}})).id
-// 			await service.update(room_id, roomPost);
-// 			await service.update(room2_id, roomPost2);
-// 			const room = await service.returnOne(room_id);
-// 			const room2 = await service.returnOne(room2_id);
-// 			expect(room.name).toEqual(roomPost.name);
-// 			expect(room2.name).toEqual(roomPost2.name);
-// 		});
-// 	  });
+	// describe('update', () => {
+	// 	it('[CHATROOM] should update 2 room', async () => {
+	// 		const roomPost = { name: unit_user + '_name', type : "random_type"};
+	// 		const roomPost2 = { name: unit_user + '_name_BIS', type : "random_type"};
+	// 		let room_id = (await repo.findOne({where: {name: unit_user}})).id
+	// 		let room2_id = (await repo.findOne({where: {name: unit_user_bis}})).id
+	// 		await service.update(room_id, roomPost);
+	// 		await service.update(room2_id, roomPost2);
+	// 		const room = await service.returnOne(room_id);
+	// 		const room2 = await service.returnOne(room2_id);
+	// 		expect(room.name).toEqual(roomPost.name);
+	// 		expect(room2.name).toEqual(roomPost2.name);
+	// 	});
+	//   });
 
-// 	describe('delete', () => {
-// 		it('should delete a room', async () => {
-// 			const room = await repo.findOneBy({name: unit_room + '_name'}); 
-// 			const id = room.id;
-// 			await service.delete(id);
-// 			await expect(service.returnOne(id)).rejects.toThrowError(
-// 				new ForbiddenException("ChatRoom not found"),
-// 				);
-// 		});
-// 	});  
-// });
+	// describe('delete', () => {
+	// 	it('should delete a room', async () => {
+	// 		const room = await repo.findOneBy({name: unit_user + '_name'}); 
+	// 		const id = room.id;
+	// 		await service.delete(id);
+	// 		await expect(service.returnOne(id)).rejects.toThrowError(
+	// 			new ForbiddenException("ChatRoom not found"),
+	// 			);
+	// 	});
+	// });  
+});
