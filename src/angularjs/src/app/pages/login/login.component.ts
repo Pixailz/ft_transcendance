7e0d86d35cd0bf795c2ec6580602c6db0ab11324
gab <ldournoi@student.42angouleme.fr>
2023-08-23 00:39:42 +0200

    [frontend] actually redirect to redirectUrl

--- src/angularjs/src/app/pages/login/login.component.ts
+++ src/angularjs/src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpXhrBackend } from '@angular/common/http';
 
 import { environment } from 'src/app/environments/environment';
+import { BackService } from 'src/app/services/back.service';
 
 @Component({
 	selector: 'app-login',
@@ -13,16 +14,30 @@ import { environment } from 'src/app/environments/environment';
 export class LoginComponent  implements OnInit {
 	code: string | null = null;
 	response: any = null;
+	state: any = null;
 
-	constructor(private route: ActivatedRoute, private http: HttpClient) { }
+	constructor(private route: ActivatedRoute, 
+				private router: Router,
+				private http: HttpClient,
+				private back: BackService) {}
 
-	ngOnInit() {
+	async ngOnInit() {
 		this.code = this.route.snapshot.queryParamMap.get('code');
-
+		this.state = this.route.snapshot.queryParamMap.get('state');
+	
 		if (this.code !== null) {
-			this.getToken();
+		  await this.getToken();
 		}
-	}
+		if (this.state !== null) {
+			if (this.state?.redirect)
+				this.router.navigate([this.state.redirect]);
+			this.state = atob(this.state);
+			this.state = JSON.parse(this.state);
+			this.state?.redirect 
+				? this.router.navigate([this.state.redirect])
+				: this.router.navigate(['/']);
+		}
+	  }
 
 	async getToken()
 	{
@@ -34,28 +49,28 @@ export class LoginComponent  implements OnInit {
 			message.style.display = 'block';
 			message.innerHTML = 'Please wait...';
 		}
-		this.http.get('/api/auth/ft_callback?code=' + this.code).subscribe(
-		(data) => {
+		await this.back.req('GET', '/auth/ft_callback?code=' + this.code)
+		.catch((error) => {
+			if (message)
+			{
+				message.innerHTML = 'Error: ' + error.statusText;
+				message.style.color = 'red';
+			}
+		})
+		.then((data) => {
 			this.response = data;
 			if (!this.response || this.response.access_token === undefined)
 				throw new Error('No access token');
 			localStorage.setItem('access_token', this.response.access_token);
 			if (this.response.status == "register")
-				window.location.href = '/register';
-			else
-				window.location.href = '/home';
-			},
-		(error) => {
-			if (message)
-				{
-					message.innerHTML = 'Error: ' + error.statusText;
-					message.style.color = 'red';
-				}
+				this.state.redirect = '/register';
 		});
 	}
 
 	SignIn()
 	{
-		window.location.href = environment.after_auth_uri;
+		window.location.href = environment.after_auth_uri 
+		+'&state='
+		+btoa(JSON.stringify({'redirect': this.route.snapshot.queryParamMap.get('returnUrl')}))
 	};
 }
