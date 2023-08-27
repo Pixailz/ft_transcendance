2268eac31ddbbdc20159ad1aa8d1ea6b6b54fbe7
Nicolas <koala@coucou.fr>
2023-08-27 16:29:11 +0200

    add unit test getAllPrivateRoom

--- src/nestjs/src/modules/database/chatRoom/service.spec.ts
+++ src/nestjs/src/modules/database/chatRoom/service.spec.ts
@@ -1,14 +1,18 @@
 import { DBChatRoomService } from "./service";
-import { ChatRoomEntity } from "./entity";
+import { ChatRoomEntity, RoomType } from "./entity";
 import { Repository } from "typeorm";
 import { Test } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { ForbiddenException } from "@nestjs/common";
 import { DBModule } from "../database.module";
+import { DBUserService } from "../user/service";
+import { DBUserChatRoomService } from "../userChatRoom/service";
 
 describe("DBChatRoomService", () => {
 	let service: DBChatRoomService;
 	let repo: Repository<ChatRoomEntity>;
+	let userService: DBUserService;
+	let userChatRoomService: DBUserChatRoomService;
 	let unit_room: string = "UNIT_ROOM";
 	let unit_room_bis = unit_room + "_BIS";
 
@@ -25,6 +29,8 @@ describe("DBChatRoomService", () => {
 		}).compile();
 
 		service = module.get<DBChatRoomService>(DBChatRoomService);
+		userService = module.get<DBUserService>(DBUserService);
+		userChatRoomService = module.get<DBUserChatRoomService>(DBUserChatRoomService);
 		repo = module.get<Repository<ChatRoomEntity>>(
 			getRepositoryToken(ChatRoomEntity),
 		);
@@ -47,9 +53,7 @@ describe("DBChatRoomService", () => {
 
 			let roomId = await service.create(roomPost);
 			let room = await service.returnOne(roomId);
-			// console.log('in create first room : room id ', room.id, 'room login = ', room.name);
 			expect(roomId).toEqual(room.id);
-
 			let roomId2 = await service.create(roomPost2);
 			let room2 = await service.returnOne(roomId2);
 			expect(roomId2).toEqual(room2.id);
@@ -58,22 +62,39 @@ describe("DBChatRoomService", () => {
 
 	describe("update", () => {
 		it("[CHATROOM] should update 2 room", async () => {
-			const roomPost = { name: unit_room + "_name", type: "random_type" };
+			const roomPost = { name: unit_room + "_name", type: RoomType.PRIVATE };
 			const roomPost2 = {
 				name: unit_room + "_name_BIS",
-				type: "random_type",
+				type: RoomType.PRIVATE,
 			};
-			let room_id = (await repo.findOne({ where: { name: unit_room } }))
-				.id;
-			let room2_id = (
-				await repo.findOne({ where: { name: unit_room_bis } })
-			).id;
+			let room_id = (await repo.findOne({ where: { name: unit_room } })).id;
+			let room2_id = (await repo.findOneBy({ name: unit_room_bis})).id;
 			await service.update(room_id, roomPost);
 			await service.update(room2_id, roomPost2);
 			const room = await service.returnOne(room_id);
 			const room2 = await service.returnOne(room2_id);
 			expect(room.name).toEqual(roomPost.name);
 			expect(room2.name).toEqual(roomPost2.name);
+			expect(room2.type).toEqual(roomPost2.type);
+		});
+	});
+
+	describe("getAllPrivateRoom", () => {
+		it("[CHATROOM] should return AllPrivateRoom", async () => {
+			let userIds = [];
+			const roomId = await service.create({name: "Unit Private Room", type: RoomType.PRIVATE});
+			for (let i: number = 0; i < 3; i++ )
+			{
+				userIds.push(await userService.create({ftLogin: "Unit Private User" + i}));
+				await userChatRoomService.create({isOwner: true}, userIds[i], roomId);
+			}
+			let roomList = await service.getAllPrivateRoom(roomId);
+			console.log('room list = ', roomList);
+			for (let i: number = 0; i < 3; i++ )
+				expect(roomList.roomInfo[i].userId).toEqual(userIds[i]);
+			for (let i: number = 0; i < 3; i++ )
+				await userService.delete(userIds[i]);
+			await service.delete(roomId);
 		});
 	});
 
