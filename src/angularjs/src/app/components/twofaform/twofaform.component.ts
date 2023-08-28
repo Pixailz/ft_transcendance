9e3ed923efd461cc744b7cf9536d85ec4084d29d
gab <ldournoi@student.42angouleme.fr>
2023-08-28 16:40:32 +0200

    [front:2fa] twofaform is now a dialog component

--- src/angularjs/src/app/components/twofaform/twofaform.component.ts
+++ src/angularjs/src/app/components/twofaform/twofaform.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute, Router, RouterState, RouterStateSnapshot } from '@angular/router';
 import { BackService } from 'src/app/services/back.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-twofaform',
@@ -11,32 +13,44 @@ export class TwofaformComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
               private router: Router,
-              private back: BackService) { }
+              private back: BackService,
+              private userService: UserService,
+              public dialogRef: MatDialogRef<TwofaformComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+
+  qrCode: string = this.data.qrCode;
+  notice: string = this.data.notice;
+  nonce: string = this.data.nonce;
 
   ngOnInit() {
   }
 
   async sendTwoFa() {
-    const nonce = this.route.snapshot.queryParamMap.get('nonce');
-    const redirect = this.route.snapshot.queryParamMap.get('returnUrl');
+    const nonce = this.route.snapshot.queryParamMap.get('nonce') || this.nonce;
     const code = document.getElementById('code') as HTMLInputElement;
-    const notice = document.getElementById('notice') as HTMLHeadingElement;
+    const status = document.getElementById('status') as HTMLParagraphElement;
+
+    if (code.value.length !== 6) {
+      status.innerText = 'Please enter a valid code.';
+      return;
+    }
+
+    let uri = '/2fa/verify/' + nonce + '/' + code.value;
+    if (this.qrCode && code.value != "")
+      uri = '/2fa/setup/' + nonce + '/' + code.value;
 
     const response = await this.back.req(
       'POST', 
-      '/auth/2fa', 
-      JSON.stringify({nonce: nonce, code: code.value}))
+      uri)
     .then((res: any) => {
-      if (res.status === 'oke') {
-        localStorage.setItem('access_token', res.access_token);
-        redirect === null ? this.router.navigate(['/']) : this.router.navigate([redirect]);
-      }
+      this.dialogRef.close(res);
     })
     .catch((err: any) => {
       console.log(err);
-      notice.innerText = err.status;
-      notice.style.display = 'block';
-      notice.style.color = 'red';
+      status.innerText = 'An error occured. Please verify your code and try again.';
+      status.style.display = 'block';
+      status.style.color = 'red';
     })
   }
 
