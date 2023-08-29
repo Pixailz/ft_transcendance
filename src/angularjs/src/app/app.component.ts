0eb93c7dfc607007cf69721a5267c5acc7e27b13
Ruben Rollin <63564987+RuBeeN-cmd@users.noreply.github.com>
2023-08-29 16:32:26 +0000

    Remove useless import

--- src/angularjs/src/app/app.component.ts
+++ src/angularjs/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
-import { slideInAnimation } from './animations';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
-	animations: [
-		slideInAnimation
-	]
 })
 
 export class AppComponent {
