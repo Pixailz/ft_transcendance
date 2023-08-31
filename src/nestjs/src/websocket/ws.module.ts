fb6b184a2bba355b5ee6e6238c8ca74719ec8c09
Pixailz <35565530+Pixailz@users.noreply.github.com>
2023-08-31 22:52:20 +0200

    108 spread websocket (fix websocket) (#121)
    [WS]
    - rename ChatGateway into Gateway
    - fix websockets + redirect
    [login:backService] fix not working request to fallback
    [setup] re not removing volumes
    
    ---------
    
    Co-authored-by: clinche <101885891+clinche@users.noreply.github.com>

--- src/nestjs/src/websocket/ws.module.ts
+++ src/nestjs/src/websocket/ws.module.ts
@@ -1,21 +1,21 @@
 import { Module } from "@nestjs/common";
 
-import { WSChatGateway } from "./chat.gateway";
+import { WSGateway } from "./ws.gateway";
 import { AuthModule } from "src/modules/auth/module";
 import { DBModule } from "src/modules/database/database.module";
 import { UserService } from "src/adapter/user/service";
 import { ChatRoomService } from "src/adapter/chatRoom/service";
-import { WSChatService } from "./chat.service";
-import { WSSocket } from "../socket.service";
+import { WSChatService } from "./chat/chat.service";
+import { WSSocket } from "./socket.service";
 
 @Module({
 	imports: [AuthModule, DBModule],
 	providers: [
-		WSChatGateway,
+		WSGateway,
 		UserService,
 		ChatRoomService,
 		WSChatService,
 		WSSocket,
 	],
 })
-export class WSChatModule {}
+export class WSModule {}
