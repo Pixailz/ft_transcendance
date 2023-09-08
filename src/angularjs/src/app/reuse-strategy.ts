e2274eba904197f0a1820e7185f7757d555af220
gab <ldournoi@student.42angouleme.fr>
2023-09-08 15:30:31 +0200

    less noise please

--- src/angularjs/src/app/reuse-strategy.ts
+++ src/angularjs/src/app/reuse-strategy.ts
@@ -4,13 +4,11 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
 	storedRouteHandles = new Map<string, DetachedRouteHandle>();
 	// Decides if the route should be stored
 	shouldDetach(route: ActivatedRouteSnapshot): boolean {
-		console.log('shouldDetach: ' + route.toString());
 		return route.data['reuseRoute'] === true;
 	}
 
 	//Store the information for the route we're destructing
 	store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-		console.log('store: ' + route.toString(), handle);
 		if (!route.routeConfig?.path)
 			return;
 		this.storedRouteHandles.set(route.routeConfig?.path, handle);
@@ -18,7 +16,6 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
 
 	//Return true if we have a stored route object for the next route
 	shouldAttach(route: ActivatedRouteSnapshot): boolean {
-		console.log('shouldAttach: ' + route.toString());
 		if (!route.routeConfig?.path)
 			return false;
 		return this.storedRouteHandles.has(route.routeConfig.path);
@@ -26,7 +23,6 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
 
 	//If we returned true in shouldAttach(), now return the actual route data for restoration
 	retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-		console.log('retrieve: ' + route.toString());
 		if (!route.routeConfig?.path)
 			return {} as DetachedRouteHandle;
 		return this.storedRouteHandles.get(route.routeConfig.path) || {} as DetachedRouteHandle;
@@ -34,7 +30,6 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
 
 	//Reuse the route if we're going to and from the same route
 	shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-		console.log('shouldReuseRoute: ' + future.toString() + ' ' + curr.toString());
 		return future.routeConfig === curr.routeConfig;
 	}
 }
