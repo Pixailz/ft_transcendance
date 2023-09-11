8121e5df692bbdf670976233fbcb148706c48aa1
gab <ldournoi@student.42angouleme.fr>
2023-09-11 08:07:07 -0400

    begin game component

--- src/angularjs/src/app/app.module.ts
+++ src/angularjs/src/app/app.module.ts
@@ -1,114 +1,117 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouteReuseStrategy } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { CodeInputModule } from 'angular-code-input';
+import { Client } from 'colyseus.js';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { AvatarComponent } from './components/avatar/avatar.component';
-import { HomeDashboardComponent } from './pages/home-dashboard/home-dashboard.component';
-import { PongComponent } from './pages/pong/pong.component';
-import { UserProfileComponent } from './pages/user-profile/user-profile.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
 import { AuthGuardService } from './services/auth-guard.service';
-import { environment } from './environments/environment';
 import { AnonymousLayoutComponent } from './layout/anonymous-layout.component';
 import { AuthenticatedLayoutComponent } from './layout/authenticated-layout.component';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { FlatButtonComponent } from './components/flat-button/flat-button.component';
-import { TwofaformComponent } from './components/twofaform/twofaform.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { CodeInputModule } from 'angular-code-input';
+import { AvatarComponent } from './components/avatar/avatar.component';
 import { ErrordialogComponent } from './components/errordialog/errordialog.component';
-import { GlobalErrorHandlerService } from './services/global-error-handler.service';
+import { FlatButtonComponent } from './components/flat-button/flat-button.component';
 import { FlatListComponent } from './components/flat-list/flat-list.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeDashboardComponent } from './pages/home-dashboard/home-dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NotificationComponent } from './components/notification/notification.component';
-import { RouteReuseStrategy } from '@angular/router';
-import { CustomReuseStrategy } from './reuse-strategy';
-
-// WEBSOCKET
-import { WSChatDmComponent } from './pages/chat-dm/chat-dm.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { PongComponent } from './pages/pong/pong.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { TwofaformComponent } from './components/twofaform/twofaform.component';
 import { WSChatChannelComponent } from './pages/chat-channel/chat-channel.component';
+import { WSChatDmComponent } from './pages/chat-dm/chat-dm.component';
+import { GlobalErrorHandlerService } from './services/global-error-handler.service';
+import { CustomReuseStrategy } from './reuse-strategy';
+import { environment } from './environments/environment';
+import { GameComponent } from './components/game/game.component';
 
 const config: SocketIoConfig = {
-	url: environment.socket_url,
-	options: {
-		path: "/ws",
-		extraHeaders: {
-			Authorization: localStorage.getItem("access_token") as string
-		},
-		autoConnect: false,
-	}
+    url: environment.socket_url,
+    options: {
+        path: "/ws",
+        extraHeaders: {
+            Authorization: localStorage.getItem("access_token") as string
+        },
+        autoConnect: false,
+    }
 };
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HeaderComponent,
-		FooterComponent,
-		HomeDashboardComponent,
-		PongComponent,
-		PageNotFoundComponent,
-		UserProfileComponent,
-		AvatarComponent,
-		LoginComponent,
-		RegisterComponent,
-		AnonymousLayoutComponent,
-		AuthenticatedLayoutComponent,
-		FlatButtonComponent,
-		TwofaformComponent,
-		ProfileComponent,
-		ErrordialogComponent,
-		FlatListComponent,
-		NotificationComponent,
-		WSChatDmComponent,
-		WSChatChannelComponent,
-	],
-	imports: [
-		BrowserModule,
-		BrowserAnimationsModule,
-		MatGridListModule,
-		MatCardModule,
-		MatMenuModule,
-		MatIconModule,
-		MatButtonModule,
-		MatDialogModule,
-		MatToolbarModule,
-		MatTableModule,
-		MatSlideToggleModule,
-		MatFormFieldModule,
-		MatInputModule,
-		MatSelectModule,
-		ReactiveFormsModule,
-		AppRoutingModule,
-		HttpClientModule,
-		FormsModule,
-		MatProgressBarModule,
-		SocketIoModule.forRoot(config),
-		CodeInputModule
-	],
-	providers: [
-		AuthGuardService,
-		{ provide: ErrorHandler, useClass: GlobalErrorHandlerService },
-		{ provide: RouteReuseStrategy, useClass: CustomReuseStrategy },
-	],
-	bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        HomeDashboardComponent,
+        PongComponent,
+        PageNotFoundComponent,
+        UserProfileComponent,
+        AvatarComponent,
+		GameComponent,
+        LoginComponent,
+        RegisterComponent,
+        AnonymousLayoutComponent,
+        AuthenticatedLayoutComponent,
+        FlatButtonComponent,
+        TwofaformComponent,
+        ProfileComponent,
+        ErrordialogComponent,
+        FlatListComponent,
+        NotificationComponent,
+        WSChatDmComponent,
+        WSChatChannelComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatGridListModule,
+        MatCardModule,
+        MatMenuModule,
+        MatIconModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatToolbarModule,
+        MatTableModule,
+        MatSlideToggleModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        MatProgressBarModule,
+        SocketIoModule.forRoot(config),
+        CodeInputModule,
+    ],
+    providers: [
+        AuthGuardService,
+        { provide: ErrorHandler, useClass: GlobalErrorHandlerService },
+        { provide: RouteReuseStrategy, useClass: CustomReuseStrategy },
+		Client,
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/angularjs/src/app/components/game/game.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/services/game.service';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+
+  constructor(
+    private gameService: GameService
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
