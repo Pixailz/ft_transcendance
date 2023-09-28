755ffbf5f456f40e8f0fc6e4aa0944098b5ae578
nfour <ycaro02@hotmail.fr>
2023-09-28 13:16:57 +0200

    refonte in progress

---
+++ src/angularjs/src/app/components/chat-view/chat-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-view',
+  templateUrl: './chat-view.component.html',
+  styleUrls: ['./chat-view.component.scss']
+})
+export class ChatViewComponent {
+
+}
