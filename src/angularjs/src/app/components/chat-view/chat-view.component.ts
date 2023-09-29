493cbc405328c3bf328071e4c85249cbf0ea8086
nfour <ycaro02@hotmail.fr>
2023-09-29 11:54:10 +0200

    finish chat view

--- src/angularjs/src/app/components/chat-view/chat-view.component.ts
+++ src/angularjs/src/app/components/chat-view/chat-view.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-chat-view',
-  templateUrl: './chat-view.component.html',
-  styleUrls: ['./chat-view.component.scss']
+	selector: 'app-chat-view',
+	templateUrl: './chat-view.component.html',
+	styleUrls: ['./chat-view.component.scss']
 })
 export class ChatViewComponent {
+	messageLength: number = 0;
+	messageForm!: FormGroup;
 
+	constructor(
+		private formBuilder: FormBuilder
+	) {}
+
+	// async ngOnInit() {
+	// 	this.messageForm = this.formBuilder.group({
+	// 		message: content
+	// 	}, { updateOn: "change" });
+
+	// 	this.messageForm.get('content')?.valueChanges
+	// 	.pipe(pairwise())
+	// 	.subscribe(([prev, next]: [any, any]) => {
+	// 		this.invalidNickname = false;
+	// 	});
+	// }
 }
