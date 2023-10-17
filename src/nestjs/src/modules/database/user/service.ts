ae7bef3928e22ac6eb040a4d95a6afc488a2c49b
nfour <ycaro02@hotmail.fr>
2023-10-17 18:43:00 +0200

    regex in replace nickname

--- src/nestjs/src/modules/database/user/service.ts
+++ src/nestjs/src/modules/database/user/service.ts
@@ -179,21 +179,7 @@ export class DBUserService {
 
     replace_nickname(name: string) : string
     {
-        const	accept_char: string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-0123456789"
-        let		new_name: string = '';
-        if (!name)
-            return (name);
-        for (let i: number = 0; i < name.length; i++)
-        {
-            for (let j:number = 0; j < accept_char.length; j++)
-            {
-                if (accept_char[j] === name[i])
-                {
-                    new_name += name[i];
-                    break;
-                }
-            }
-        }
-        return (new_name);
+		const regexp = new RegExp(/(?:[^a-zA-Z0-9-_]*)/g);
+        return name.replaceAll(regexp, '');
     }
 }
